2e3a979a3a5b454b9b706e252bb019e3bedf9552
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-01 14:04:30 +0300

    added a MSVTF

--- table-valued-function.sql
+++ table-valued-function.sql
@@ -1,69 +1,60 @@
-/* CREATE FUNCTION production.getBrands()
-RETURNS @brands TABLE (
-	brand_id INT,
-	brand_name VARCHAR(255)
-)
+/* CREATE FUNCTION production.getCategories()
+RETURNS TABLE
+AS
+RETURN
+    SELECT *
+    FROM production.categories */
+
+
+--  SELECT * FROM production.getCategories()
+
+-- DROP FUNCTION production.getCategories
+
+/* CREATE FUNCTION production.getProductsByCategories(@category VARCHAR(255))
+RETURNS TABLE
+AS
+RETURN
+    SELECT p.product_id, p.product_name
+    FROM production.products p
+    JOIN production.categories c ON p.category_id = c.category_id
+    WHERE c.category_name = @category */
+
+
+-- SELECT * FROM production.getProductsByCategories('Road Bikes')
+
+-- DROP FUNCTION IF EXISTS production.getProductsByCategories
+
+
+/* CREATE FUNCTION dbo.users()
+RETURNS @users TABLE (
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(255),
+        phone VARCHAR(25),
+        user_type VARCHAR(20)
+    )
 AS
 BEGIN
-	INSERT INTO @brands
-	SELECT brand_id, brand_name
-	FROM production.brands;
-	
-	RETURN;
-END */
+    INSERT INTO @users
+    SELECT 
+        first_name, 
+        last_name, 
+        email, 
+        phone,
+        'Staff'
+    FROM
+        sales.staffs;
 
-
--- SELECT * FROM production.GetBrands() 
-
--- DROP FUNCTION IF EXISTS dbo.GetBrands;
-
-
-/* CREATE FUNCTION production.getProductsByCategory(@category_name VARCHAR(255))
-RETURNS 
-      @Result TABLE (
-            product_id INT,
-            product_name VARCHAR(255),
-            brand_id INT,
-            category_id INT,
-            model_year SMALLINT,
-            list_price DECIMAL(10, 2)
-      )
-      AS
-      BEGIN
-            INSERT INTO @Result (product_id, product_name, brand_id, category_id, model_year, list_price)
-            SELECT p.product_id, p.product_name, p.brand_id, p.category_id, p.model_year, p.list_price
-            FROM production.products p
-            JOIN production.categories c ON p.category_id = c.category_id
-            WHERE c.category_name = @category_name;
-RETURN;
-END */
-
-
-/* ALTER FUNCTION production.getProductsByCategory(@category_name VARCHAR(255))
-RETURNS 
-      @Result TABLE (
-            product_id INT,
-            product_name VARCHAR(255),
-            brand_id INT,
-            category_id INT,
-            list_price DECIMAL(10, 2)
-      )
-      AS
-      BEGIN
-            INSERT INTO  @Result (product_id, product_name, brand_id, category_id, list_price)
-            SELECT TOP 5 p.product_id, p.product_name, p.brand_id, p.category_id, p.list_price
-            FROM production.products p
-            JOIN production.categories c ON p.category_id = c.category_id
-            WHERE c.category_name = @category_name;
-RETURN;
-END */
-
-SELECT COUNT(*) FROM production.getProductsByCategory('Road Bikes');
-
--- DROP FUNCTION production.getProductsByCategory
-
-
-
-
-
-
+    INSERT INTO @users
+    SELECT 
+        first_name, 
+        last_name, 
+        email, 
+        phone,
+        'Customer'
+    FROM
+        sales.customers;
+    RETURN;
+END;
+ */
+-- DROP FUNCTION IF EXISTS dbo.users
